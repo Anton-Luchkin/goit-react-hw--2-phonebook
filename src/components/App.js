ef6d63728f98e1b5a4dec8d0130fe72bc9aacad0
Антон Лучкин <luchkin1988@gmail.com>
2020-09-12 13:53:20 +0300

    hw-03-phonebook

--- src/components/App.js
+++ src/components/App.js
@@ -9,27 +9,43 @@ import ShortId from 'shortid';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
-  addContact = data => {
-    const message = `${data.name} is already in contacts`;
-    for (const contact of this.state.contacts) {
-      if (data.name === contact.name) {
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    this.setState({ contacts: parsedContacts });
+
+    if (parsedContacts) {
+      this.setState({ todos: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevState) {
+    const { contacts } = this.state;
+    if (contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+    }
+  }
+
+  addContact = ({ name, number }) => {
+    const { contacts } = this.state;
+
+    const message = `${name} is already in contacts`;
+
+    for (const contact of contacts) {
+      if (name === contact.name) {
         alert(message);
         return;
       }
     }
+
     const contact = {
       id: ShortId.generate(),
-      name: data.name,
-      number: data.number,
+      name,
+      number,
     };
 
     this.setState(({ contacts }) => {
@@ -37,13 +53,13 @@ export class App extends Component {
     });
   };
 
-  changeFilter = e => {
-    this.setState({ filter: e.target.value });
+  changeFilter = ({ target }) => {
+    this.setState({ filter: target.value });
   };
 
   getVisibleContacts = () => {
     const { contacts, filter } = this.state;
-    const normalizedFilter = filter.toLocaleLowerCase();
+    const normalizedFilter = filter.toLowerCase();
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(normalizedFilter),
     );
